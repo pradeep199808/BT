db0c9bd96716e83b3875272ee250361a64c9123a
bhavaniaddanki <bhavaniaddanki14@gmail.com>
2021-11-05 16:48:19 +0530

    merging

---
+++ stash.sh
@@ -0,0 +1,5 @@
+This is the stash file......
+for updating purpose
+This file updating fo cherrypick
+merge the code
+
--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ hiiiii
 hlooooooooooooooooooooooooooooooooooooooo
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 This file is used for switch to the another branch.
+newly updating
+merging the code one branch to another
 
 
 12458273487
